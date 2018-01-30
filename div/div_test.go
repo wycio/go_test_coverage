92f7010288c6bc80ab528914fb92ce4cdc8c3318
Waqas Younas <waqas@customer.io>
2018-01-30 21:30:32 +0500

    Added more tests for div

--- div/div_test.go
+++ div/div_test.go
@@ -8,3 +8,10 @@ func TestDiv(t *testing.T) {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 	}
 }
+
+func TestMultiply(t *testing.T) {
+	total := Multiply(10, 5)
+	if total != 50 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 50)
+	}
+}
